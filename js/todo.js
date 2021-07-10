a1113e95b3aea26592edcc5ecb15e0ad51da3bec
youredith <83350573+youredith@users.noreply.github.com>
2021-07-10 21:32:54 +0900

    for JH

--- js/todo.js
+++ js/todo.js
@@ -17,12 +17,12 @@ function deleteToDo(event){
     saveToDos();
 }
 
-/*
+
 function checkCheckBox(event){
-    const checkCover = document.getElementById("check-box");
+    const checkCover = event.target.parentElement;
+    console.log(event.target.parentElement);
     checkCover.innerText = "∨";
 }
-*/
 
 function paintToDo(newToDo){
     const li = document.createElement("li");
@@ -34,17 +34,17 @@ function paintToDo(newToDo){
     deleteButton.innerText = "–";
     deleteButton.addEventListener("click", deleteToDo);
     
-    /*
+    
     const createCheckBox = document.createElement("button");
     createCheckBox.setAttribute("id", "check-box");
     const checkBox = document.getElementById("check-box");
     checkBox.innerText = "▢";
     checkBox.addEventListener("click", checkCheckBox);
-    */
+    
 
     li.appendChild(span);
     li.appendChild(deleteButton);
-    // li.appendChild(createCheckBox); 
+    li.appendChild(createCheckBox); 
     toDoList.appendChild(li);
 }
 
