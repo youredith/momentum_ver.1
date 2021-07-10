51596fc1e0adbc9a01f52e8b9bc770e41ff82f89
youredith <83350573+youredith@users.noreply.github.com>
2021-07-10 21:11:24 +0900

    before the todo problem

---
+++ js/todo.js
@@ -0,0 +1,73 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+/*
+function checkCheckBox(event){
+    const checkCover = document.getElementById("check-box");
+    checkCover.innerText = "∨";
+}
+*/
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    li.id = newToDo.id;  
+    const span = document.createElement("span");
+    span.innerText = newToDo.text;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "–";
+    deleteButton.addEventListener("click", deleteToDo);
+    
+    /*
+    const createCheckBox = document.createElement("button");
+    createCheckBox.setAttribute("id", "check-box");
+    const checkBox = document.getElementById("check-box");
+    checkBox.innerText = "▢";
+    checkBox.addEventListener("click", checkCheckBox);
+    */
+
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    // li.appendChild(createCheckBox); 
+    toDoList.appendChild(li);
+}
+
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const NewToDo = toDoInput.value; 
+    toDoInput.value = "";
+    
+    const newToDoObj = {
+        text : NewToDo,
+        id : Date.now(),
+    };
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
+    saveToDos();    
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
