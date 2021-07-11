b8a0b1ea8707019fbaffe03a49759d167c7d9fa3
youredith <83350573+youredith@users.noreply.github.com>
2021-07-11 18:27:48 +0900

    in solving dday

--- js/dday.js
+++ js/dday.js
@@ -1,13 +1,15 @@
 const userDate = document.querySelector("#userdate");
 const userDDay = document.querySelector("#user-d-day");
 const HIDDEN_CLASSNAME = "hidden";
+const SUBMITTED_DATE_KEY = "submitted-date";
 
 function dDay(event) {
   event.preventDefault();
   userDate.classList.add(HIDDEN_CLASSNAME);  
   const userSubmittedDate = userDate.value;
-  localStorage.setItem(submitted-date,userSubmittedDate);
-  paintDDay();  
+  
+  localStorage.setItem(SUBMITTED_DATE_KEY, userSubmittedDate);
+  paintDDay(userSubmittedDate);  
 }
 
 function paintDDay(userSubmittedDate){
@@ -23,5 +25,13 @@ function paintDDay(userSubmittedDate){
 
 
 setInterval(dDay, 1000*60*60*24);
-userDate.addEventListener("click", dDay);
 
+
+const savedUserDate = localStorage.getItem(SUBMITTED_DATE_KEY);
+
+if(savedUserDate === null){
+    userDate.classList.remove(HIDDEN_CLASSNAME);
+    userDate.addEventListener("submit", dDay);
+} else {
+  paintDDay(savedUserDate);
+}
